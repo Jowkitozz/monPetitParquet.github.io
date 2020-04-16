1e47d31634cb130dc14bafda430d6b62ff169c95
Dams <deams.work@gmail.com>
2020-04-16 15:01:28 +0200

    Ajout taille icones + manifest + services worker

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'mpp-cache-v1';
+
+const FILES_TO_CACHE = [
+'/index.html'];
+
+var FILES_TO_CACHE = [
+    './',
+    './index.html'
+];
+  
+self.addEventListener('install', (evt) => {
+    console.log('[ServiceWorker] Install');
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log('[ServiceWorker] Pre-caching offline page');
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+    self.skipWaiting();
+  });
+  
